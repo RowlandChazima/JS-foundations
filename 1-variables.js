1733a28c587187d265a819ca8f34f023fb25381b
Rowland Chazima <apungurowland90@gmail.com>
2026-04-23 17:23:44 +0300

    Explanation on JS variables

---
+++ 1-variables.js
@@ -0,0 +1,26 @@
+// Imagine that its the end of the month or the end of a week and you're out of foodstuff and you decide to go to the supermarket to buy them........
+
+// What do you do? You make a list of the things you want to buy and then you go to the supermarket and buy them. And of course as you're shopping you wont be able to carry all of the items you need by hand.......
+
+// What do you do in this case? You decide to  get a shopping cart to put and carry all the items you want to buy.
+
+// In JavaScript, the act of deciding to get a shopping cart is making a DECLARATION whereby now on your computer you're just telling it "Hey, I need some space to store some stuff"
+
+// In this case, the shopping cart is the variable--WHERE YOU CAN PUT ALL OF THE FOODSTUFF YOU WANNA BUY.
+
+// And the food stuff is the  value--THE ACTUAL THINGS YOU WANT TO BUY. IN JS THIS WILL THE VALUES ARE TERMED AS DATA TYPES.
+
+// So in JS, a variable is a container for storing data values.
+
+// There are three ways of declaring a variable in JavaScript: using 'var', 'let', or 'const'.
+
+// 1...WHERE 'let' is used to declare a variable(value) that can be reassigned later, what do i mean when that can be reassigned later? Let's say that you've bought 5-Litres of milk and it costs Ksh 200, in JS it would be written as:
+let milk = 200;
+
+// Now let's say you want to you spot a better deal for the same quantity of milk at Ksh 150, Of course without rethinking you would opt for the one that is cheaper..... So you've made a change where youve reassigned your milk(variable name) a different value which would be re-written as:
+let milk = 150;
+
+// 2...WHERE 'const' is used to declare a variable(value) that cannot be reassigned later, what do i mean when that cannot be reassigned later? Let's say that you've bought 5-Litres of milk and it costs Ksh 200 and there's no discount ama haiko kwa offer ... so ni lazima uinunue na hiyo bei ju unaihitaji, in JS it would be written as:
+const milk = 200;
+
+// 3....'var' is the old way of declaring a variable and it is not used that much cause it can bring some errors in your code....
